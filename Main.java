f8f9a7bae91293df63c5c706101c45e17dfbc070
YourNerdiness <darcyjsutton7@gmail.com>
2023-07-27 07:57:13 +1000

    implemented calculatewQValues function

--- Main.java
+++ Main.java
@@ -13,44 +13,70 @@ public class Main<State, Action> {
 
     }
 
-    private double calculateQValues(double prevQValue, double reward, State state) {
+    private double calculateQValues(double prevQValue, double reward, State oldState, Action actionTaken, State newState) {
 
-        HashMap<Action, Double> actionsInThisState = this.qTable.get(state);
+        HashMap<Action, Double> actionsInNewState = this.qTable.get(newState);
 
-        if (actionsInThisState == null) {
+        if (actionsInNewState == null) {
 
             throw new RuntimeException("Current state has not been intilized.");
 
         }
 
-        Set<Action> actions = actionsInThisState.keySet();
+        Set<Action> actions = actionsInNewState.keySet();
 
-        double maxQValue = Double.NEGATIVE_INFINITY;
-        Action maxQValueAction;
+        double maxFutureReward = Double.NEGATIVE_INFINITY;
 
         for (Action action : actions) {
 
-            Double actionQValue = actionsInThisState.get(action);
+            Double actionQValue = actionsInNewState.get(action);
 
             if (actionQValue == null) {
 
-                actionsInThisState.replace(action, 0.0);
+                actionsInNewState.put(action, 0.0);
                 actionQValue = 0.0;
 
             }
 
-            if (actionQValue > maxQValue) {
+            if (actionQValue > maxFutureReward) {
 
-                maxQValue = actionQValue;
-                maxQValueAction = action;
+                maxFutureReward = actionQValue;
 
             }
 
         }
 
-        double maxFutureReward = 0.0;
+        HashMap<Action, Double> oldStateActions = this.qTable.get(oldState);
 
-        return maxFutureReward;
+        Double oldQValue;
+
+        if (oldStateActions == null) { 
+            
+            oldQValue = 0.0;
+
+            this.qTable.put(oldState, new HashMap<Action, Double>());
+        
+        }
+
+        else {
+
+            Double unchekcedOldQValue = oldStateActions.get(actionTaken);
+
+            if (unchekcedOldQValue == null) {
+
+                oldQValue = 0.0;
+
+            }
+
+            else {
+
+                oldQValue = unchekcedOldQValue;
+
+            }
+
+        }
+
+        return oldQValue + alpha * (reward + gamma * maxFutureReward - oldQValue);
 
     }
 
